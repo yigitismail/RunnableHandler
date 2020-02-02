5192abb05c42ddc90315c880b9bd7431f6f013e7
yigitismail <smlygt@icloud.com>
2020-02-03 00:28:14 +0300

    failed

--- app/src/main/java/com/yigitismail/runnablehandler/MainActivity.java
+++ app/src/main/java/com/yigitismail/runnablehandler/MainActivity.java
@@ -3,12 +3,50 @@ package com.yigitismail.runnablehandler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int number;
+    Runnable runnable;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        number = 0;
+
+
+    }
+
+    public void start(View view){
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                textView.setText("Number: " + number);
+                number++;
+                textView.setText("Number: " + number);
+
+                handler.postDelayed(runnable,1000);
+
+            }
+        };
+
+        handler.post(runnable);
+
+
+    }
+
+    public void stop(View view){
+
     }
 }
